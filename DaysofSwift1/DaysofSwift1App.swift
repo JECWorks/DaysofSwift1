9710bd21ef70345514e2cf2bedb12654fc482224
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-06-09 21:52:17 -0400

    Initial Commit

---
+++ DaysofSwift1/DaysofSwift1App.swift
@@ -0,0 +1,17 @@
+//
+//  DaysofSwift1App.swift
+//  DaysofSwift1
+//
+//  Created by Jason Cox on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct DaysofSwift1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
